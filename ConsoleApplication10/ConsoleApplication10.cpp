e3bc7e23743ff181ceabbf354353ec4bf35a18f4
KandhanSekar <kandhan3694@gmail.com>
2016-07-27 15:39:12 -0700

    first func... still more to go !

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -2,10 +2,29 @@
 //
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
+char box[3][3] = {'1','2','3','4','5','6','7','8','9'};
+void printbox()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << box[i][j]<<"\t";
+		}
+		cout << "\n";
+	}
+}
 
+void enterval()
+{
 
+}
 int main()
 {
+	printbox();
+	enterval();
     return 0;
 }
 
