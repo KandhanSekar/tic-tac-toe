031c8a0da50771428c27a722c8864ea93ba48d7a
KandhanSekar <kandhan3694@gmail.com>
2016-07-28 15:14:37 -0700

    tictactoe great progress!!

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -4,25 +4,70 @@
 #include "stdafx.h"
 #include<iostream>
 using namespace std;
-char box[3][3] = {'1','2','3','4','5','6','7','8','9'};
+char box[] = {'0','1','2','3','4','5','6','7','8','9'};
 void printbox()
 {
-	for (int i = 0; i < 3; i++)
+	int i = 1;
+	while (box[i] != 0)
 	{
-		for (int j = 0; j < 3; j++)
+		cout << box[i]<<"\t";
+		i++;
+		if (i % 3 == 1)
 		{
-			cout << box[i][j]<<"\t";
+			cout << "\n";
 		}
-		cout << "\n";
 	}
 }
-
+void printtnstr()
+{
+	cout << "WELCOME TO KANDY'S TIC TAC TOE" << "\n";
+	cout << "The Grid shows you the position number of each blank space,\n indicate the position number where you want to place your mark during your turn\n";
+}
 void enterval()
 {
+	int pos,play=1;
+	while (true)
+	{
+		if (play == 1)
+		{
+			cout << "Player one!!! Enter your position number";
+			cin >> pos;
+			if (box[pos] == 'x' || box[pos] == 'o')
+			{
+				cout << "Invalid !! try again";
+				enterval();
+			}
+			else
+			{
+				box[pos] = 'x';
+				play = 2;
+				printbox();
+			}
+			
+		}
+		else
+		{
+			cout << "Player two!!! Enter your position number";
+			cin >> pos;
+			if (box[pos] == 'x' || box[pos] == 'o')
+			{
+				cout << "Invalid !! try again";
+				enterval();
+			}
+			else
+			{
+				box[pos] = 'o';
+				play = 1;
+				printbox();
+			}
 
+		}
+		
+	}
 }
 int main()
 {
+	printtnstr();
 	printbox();
 	enterval();
     return 0;
